bcdf664e5383a2663b42bb40bb799aa498285bc0
Hunter Hays <hhays@verizonmedia.com>
2020-09-21 09:57:00 -0700

    Swift Package

--- Flurry Ads API Documentation/files.js
+++
@@ -1,4 +0,0 @@
-var files =
-[
-    [ "FlurrySDK", "dir_e5245282c438ec9c78fc7f0be203a857.html", "dir_e5245282c438ec9c78fc7f0be203a857" ]
-];
--- Flurry Analytics API Documentation/files.js
+++
@@ -1,4 +0,0 @@
-var files =
-[
-    [ "FlurrySDK", "dir_4ed32c60bb05fe79fa4a5b2fb8e808ee.html", "dir_4ed32c60bb05fe79fa4a5b2fb8e808ee" ]
-];
